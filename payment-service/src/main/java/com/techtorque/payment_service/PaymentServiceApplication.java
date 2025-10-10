c0d44c94b5ccdc463b8c614108fb5bbd3cd0a991
RandithaK <bharkula3@gmail.com>
2025-10-11 00:22:17 +0530

    feat: Update SecurityConfig to disable form login and HTTP Basic authentication, and add Swagger/OpenAPI whitelist

--- payment-service/src/main/java/com/techtorque/payment_service/PaymentServiceApplication.java
+++ payment-service/src/main/java/com/techtorque/payment_service/PaymentServiceApplication.java
@@ -2,8 +2,9 @@ package com.techtorque.payment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class PaymentServiceApplication {
 
 	public static void main(String[] args) {
