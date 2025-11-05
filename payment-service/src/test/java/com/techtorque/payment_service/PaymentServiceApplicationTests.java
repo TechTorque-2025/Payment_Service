f17ede05094ce7ea0f0271def0d2d331a5261087
RandithaK <bharkula3@gmail.com>
2025-11-05 21:28:23 +0530

    test: Update application tests

--- payment-service/src/test/java/com/techtorque/payment_service/PaymentServiceApplicationTests.java
+++ payment-service/src/test/java/com/techtorque/payment_service/PaymentServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.techtorque.payment_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PaymentServiceApplicationTests {
 
 	@Test
